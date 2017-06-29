5a47107b431b6ceec2da72aac9f559dff6ba0f80
Jordan Buttkevitz <jbuttkevitz@wombatsecurity.com>
2017-06-29 11:53:40 -0400

    updated

--- app/workflowApp.py
+++ app/workflowApp.py
@@ -11,8 +11,11 @@ import distutils.core
 import subprocess
 import requests
 
+
 home = os.path.expanduser("~")
-INVENTORY_URL = "https://raw.githubusercontent.com/enigma-io/workflow-interview-challenge/master/inventory.tsv"
+INVENTORY_URL = ("https://raw.githubusercontent.com/enigma-io/" +
+                 "workflow-interview-challenge/master/inventory.tsv")
+
 
 def logger_config():
 
@@ -39,21 +42,75 @@ def parse_args_config():
     return parser
 
 
+def _output_to_json_file(output_filename, textfile):
+
+    try:
+        with open(output_filename, "w") as outfile:
+            reader = csv.DictReader(textfile.splitlines(),
+                                    delimiter='\t')
+            json.dump(list(reader), outfile, ensure_ascii=False,
+                      sort_keys=True, indent=4)
+    except IOError as io:
+        logging.error("An error has occurred with writing file, %s", io)
+
+
 def get_inventory(url):
 
     try:
         response = requests.get(url)
         response.raise_for_status()
-        inventoryfile_text = response.text
-        reader = dict()
-        with open("data.json", "w") as outfile:
-            reader = csv.DictReader(inventoryfile_text.splitlines(), delimiter='\t')
-        json2 = json.dumps(reader, ensure_ascii=False, sort_keys=True, indent=4)
-
+        _output_to_json_file("data.json", response.text)
     except ValueError as ve:
         logging.error("An error with requesting api, %s", ve)
     except requests.exceptions.HTTPError as he:
         logging.error("An httperror has occurred, %s", he)
+
+
+# [{
+# "Volume 2015": {
+#   "Minimum": "2",
+#   "Maximum": "32",
+#   "Median": "36"
+# }
+# }]
+#
+def process_inventory(filename):
+    with open(filename, "r") as jsonfile:
+        inventory = json.load(jsonfile, encoding="utf-8")
+    return inventory
+
+
+def run_statistics_on_column(inventory, column_name):
+    values = list()
+    for row_dict in inventory:
+        values.append(row_dict.get(column_name))
+    sorted_values = sorted(values, key=float, reverse=True)
+    values_dict = dict([("Maximum", str(_max(sorted_values))),
+                        ("Minimum", str(_min(sorted_values))),
+                        ("Median", str(_median(sorted_values)))])
+    with open("stats.json", "w") as outfile:
+        json.dump(values_dict, outfile)
+
+
+def _median(sorted_values):
+    result = 0.0
+    size = len(sorted_values)
+    is_even = (size % 2 == 0)
+    if is_even:
+        hi = float(sorted_values[size / 2])
+        lo = float(sorted_values[(size / 2) - 1])
+        result = (hi + lo) / 2
+    else:
+        result = float(sorted_values[size / 2])
+    return result
+
+
+def _max(sorted_values):
+    return float(sorted_values[0])
+
+
+def _min(sorted_values):
+    return float(sorted_values[len(sorted_values) - 1])
 
 
 def main():
@@ -63,7 +120,9 @@ def main():
     args = parser.parse_args()
     args_list = vars(args)
     try:
-        json = get_inventory(INVENTORY_URL)
+        get_inventory(INVENTORY_URL)
+        inventory = process_inventory("data.json")
+        run_statistics_on_column(inventory, "Volume 2015")
         logging.info("It is complete.")
     except ValueError as e:
         print(e.args)
