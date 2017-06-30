8b8ec2ae38caeb2786d0b7c4f1e58f87a3db905f
Jordan Buttkevitz <jbuttkevitz@wombatsecurity.com>
2017-06-30 00:41:29 -0400

    first draft working before revisions

--- src/workflowapp/workflow.py
+++ src/workflowapp/workflow.py
@@ -15,12 +15,6 @@ from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 
 
-HOME_PATH = os.path.expanduser("~")
-INVENTORY_URL = ("https://raw.githubusercontent.com/enigma-io/" +
-                 "workflow-interview-challenge/master/inventory.tsv")
-DEFAULT_CONFIG_PATH = "../../config/workflows.json"
-
-
 def logger_config():
     logging.basicConfig(level=logging.INFO, format=("[###%(levelname)s] "
                                                     "%(asctime)s: "
@@ -32,8 +26,11 @@ def logger_config():
 
 
 def load_settings(filepath):
-    logging.info("Loading configuation settings.")
+    if filepath is None:
+        raise TypeError
+    logging.info("Loading configuation settings from %s", filepath)
     settings = dict()
+
     try:
         with open(filepath, "r") as f:
             settings = json.load(f)
@@ -43,9 +40,14 @@ def load_settings(filepath):
 
 
 def get_active_workflow(settings):
-    logging.info("Loading active workflow.")
+    if sorted_values is None:
+        raise TypeError
+    logging.info("Loading active workflow")
     result = 0
+
     for value in settings:
+        if value is None:
+            raise TypeError
         if value.get("active") is True:
             result = value.get("id")
             break
@@ -53,6 +55,10 @@ def get_active_workflow(settings):
 
 
 def _output_to_json_file(output_filename, textfile):
+    if output_filename is None or textfile is None:
+        raise TypeError
+    logging.info("Reading file into the json file %s", output_filename)
+
     try:
         with open(output_filename, "w") as outfile:
             reader = csv.DictReader(textfile.splitlines(),
@@ -63,7 +69,11 @@ def _output_to_json_file(output_filename, textfile):
         logging.error("An error has occurred with writing file, %s", io)
 
 
-def get_inventory(url):
+def get_inventory(url, jsonfile):
+    if url is None or jsonfile is None:
+        raise TypeError
+    logging.info("Retrieve http request payload from %s", url)
+
     try:
         session = requests.Session()
         session_retries = Retry(total=3,
@@ -71,7 +81,7 @@ def get_inventory(url):
                 status_forcelist=[ 500, 502, 503, 504 ])
         session.mount('http://', HTTPAdapter(max_retries=session_retries))
         response = session.get(url)
-        _output_to_json_file("data.json", response.text)
+        _output_to_json_file(jsonfile, response.text)
     except ValueError as ve:
         logging.error("An error with requesting api, %s", ve)
     except requests.exceptions.HTTPError as he:
@@ -81,14 +91,29 @@ def get_inventory(url):
 
 
 def process_inventory(filename):
-    with open(filename, "r") as jsonfile:
-        inventory = json.load(jsonfile, encoding="utf-8")
-    return inventory
+    if filename is None:
+        raise TypeError
+    logging.info("Loading %s", filename)
+
+    try:
+        with open(filename, "r") as jsonfile:
+            inventory = json.load(jsonfile)
+        return inventory
+    except IOError as io:
+        logging.error("An error has occurred with loading file, %s", io)
 
 
-def run_statistics_on_column(inventory, column_name):
+def run_statistics_on_column(inventory, column_name, jsonfile):
+    if inventory is None or column_name is None or jsonfile is None:
+        raise TypeError
+    logging.info("Run stats against column %s from the file %s",
+                 column_name, jsonfile)
+
     values = list()
+
     def _median(sorted_values):
+        if sorted_values is None:
+            raise TypeError
         result = 0.0
         size = len(sorted_values)
         is_even = (size % 2 == 0)
@@ -101,30 +126,64 @@ def run_statistics_on_column(inventory, column_name):
         return result
 
     def _max(sorted_values):
+        if sorted_values is None:
+            raise TypeError
         return float(sorted_values[0])
 
     def _min(sorted_values):
+        if sorted_values is None:
+            raise TypeError
         return float(sorted_values[len(sorted_values) - 1])
 
     for row_dict in inventory:
+        if row_dict is None:
+            raise TypeError
         values.append(row_dict.get(column_name))
+
     sorted_values = sorted(values, key=float, reverse=True)
     values_dict = dict([("Maximum", str(_max(sorted_values))),
                         ("Minimum", str(_min(sorted_values))),
                         ("Median", str(_median(sorted_values)))])
-    with open("stats.json", "w") as outfile:
-        json.dump(values_dict, outfile)
+
+    try:
+        with open(jsonfile, "w") as outfile:
+            json.dump(values_dict, outfile)
+    except IOError as io:
+        logging.error("An error has occurred with writing file, %s", io)
+
+
+def print_filesize(directory):
+    if directory is None:
+        raise TypeError
+
+    try:
+        files = os.listdir(directory)
+        for a_file in files:
+            print "file: ", a_file, ", size: ", os.path.getsize(directory + a_file)
+    except IOError as io:
+        logging.error("An error has occurred with writing file, %s", io)
 
 
 def run():
     logger_config()
+    DEFAULT_CONFIG_PATH = "../../config/workflows.json"
     settings = load_settings(DEFAULT_CONFIG_PATH)
-    ACTIVE_WORKFLOWID = get_active_workflow(settings)
 
     try:
-        get_inventory(INVENTORY_URL)
-        inventory = process_inventory("data.json")
-        run_statistics_on_column(inventory, "Volume 2015")
-        logging.info("It is complete.")
+        DEFAULT_WORKFLOW = settings[0].get("operations")
+        OUTPUT_DIR = "../../output/"
+        INV_FILENAME = DEFAULT_WORKFLOW[0].get("filename")
+        INVENTORY_URL = DEFAULT_WORKFLOW[0].get("input")
+        INV_JSON_FILE = DEFAULT_WORKFLOW[1].get("filename")
+        RUN_STATS_ON_COLUMN_NAME = DEFAULT_WORKFLOW[2].get("extra")[0].get("column_name")
+        STATS_JSON_FILE = DEFAULT_WORKFLOW[2].get("filename")
+
+        get_inventory(INVENTORY_URL + INV_FILENAME, OUTPUT_DIR + INV_JSON_FILE)
+        inventory = process_inventory(OUTPUT_DIR + INV_JSON_FILE)
+        run_statistics_on_column(inventory, RUN_STATS_ON_COLUMN_NAME,
+                                 OUTPUT_DIR + STATS_JSON_FILE)
+        print_filesize(OUTPUT_DIR)
     except ValueError as e:
         logging.error("An error has occurred, %s", e)
+    except TypeError as te:
+        logging.error("An error has occurred, %s", te)
